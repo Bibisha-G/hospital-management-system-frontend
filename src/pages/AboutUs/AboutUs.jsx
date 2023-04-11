4968cc7ef4a97569a3176a7b90d9a04e1f53bd97
Bibisha-G <bibishaguragai656@gmail.com>
2023-04-12 01:35:13 +0500

    Fixed Css

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -128,28 +128,24 @@ function AboutUs() {
                             <div className="feature-container feature-bx3">
                                 <h2 className="counter">120</h2>
                                 <h5 className="ttr-title">Years With You</h5>
-                                <p>Etiam ante ante, molestie vitae cursus ac, pharetra euismod libero.</p>
                             </div>
                         </div>
                         <div className="col-xl-3 col-sm-6 mb-30">
                             <div className="feature-container feature-bx3">
                                 <h2 className="counter">400</h2>
                                 <h5 className="ttr-title">Awards</h5>
-                                <p>Etiam ante ante, molestie vitae cursus ac, pharetra euismod libero.</p>
                             </div>
                         </div>
                         <div className="col-xl-3 col-sm-6 mb-30">
                             <div className="feature-container feature-bx3">
                                 <h2 className="counter">250</h2>
                                 <h5 className="ttr-title">Doctors</h5>
-                                <p>Etiam ante ante, molestie vitae cursus ac, pharetra euismod libero.</p>
                             </div>
                         </div>
                         <div className="col-xl-3 col-sm-6 mb-30">
                             <div className="feature-container feature-bx3">
                                 <h2 className="counter">800</h2>
                                 <h5 className="ttr-title">Satisfied Client</h5>
-                                <p>Etiam ante ante, molestie vitae cursus ac, pharetra euismod libero.</p>
                             </div>
                         </div>
                     </div>
